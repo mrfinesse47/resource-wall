17298c4f3b64c8a06cc80c66ef2378afd781c154
Jonathan Su <jsu6898@gmail.com>
2021-11-27 12:35:53 -0800

    prebranch

--- public/scripts/views/pins.js
+++ public/scripts/views/pins.js
@@ -1,5 +1,4 @@
 const pins = () => {
-  $("#pins-container").empty();
 
   //and then it appends everything to the screen
   //maybe we do a foreach loop
@@ -14,15 +13,6 @@ const pins = () => {
     alert("12345 clicked");
   });
 
-  //we have to track all even listeners
-
-  Window.allEventListeners.pins = ["#12345"]; //this would be a whole list of ids, the same would happen for other components
-
-  //here we can remove event listeners, using the jquery off() function
-
-  $(Window.allEventListeners.pins[0]).off();
-
-  console.log(Window.allEventListeners.pins[0]);
   //we can append as many as we want.
   $("#pins-container").append($pin);
   $("#pins-container").append($pin);
