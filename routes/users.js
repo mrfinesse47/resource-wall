51f3b51592b92985b449895686fe4f8261e000ca
Kevin Mason <krmason2020@gmail.com>
2021-11-28 05:07:16 +0000

    got login/logout working in users routes

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@
 
 const express = require("express");
 const router = express.Router();
-const fakeUser = require("../fake-data/user.json");
+//const fakeUser = require("../fake-data/user.json");
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
@@ -20,25 +20,36 @@ module.exports = (db) => {
     //     res.status(500).json({ error: err.message });
     //   });
     res.json({
-      users: "hello"
+      users: "hello",
     });
   });
-
-  router.post("/signup", (req, res) => {
-    console.log("in router", req.body);
-    res.json({
-      users: "hello"
-    });
-  });
-
-
   router.post("/login", (req, res) => {
-    console.log("in router", req.body);
-    res.json({
-      users: "hello"
+    db.getUserByEmail(req.body.email)
+      .then((user) => {
+        if (user) {
+          if (req.body.password === user.password) {
+            req.session.user_id = user.id;
+            console.log("authenticated");
+            res.json({ auth: true, message: "success" });
+          } else {
+            console.log("not authenticated");
+            res.json({ auth: false, message: "bad password" });
+          }
+        } else {
+          console.log("email does not exist");
+          res.json({ auth: false, message: "not a valid email address" });
+        }
+      })
+      .catch((err) => {
+        console.log("error");
+        console.log(err);
+        res.status(500).json({ auth: false, message: "internal server error" });
+      });
+
+    router.post("/logout", (req, res) => {
+      req.session = null; //deletes user cookies
+      res.json({ auth: false });
     });
   });
   return router;
-
-
 };
