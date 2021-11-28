635a0411157c724fcc6ff0f8750ab15a1443d8ef
Kevin Mason <krmason2020@gmail.com>
2021-11-28 22:29:13 +0000

    succesfully created registration route

--- routes/users.js
+++ routes/users.js
@@ -27,23 +27,13 @@ module.exports = (db) => {
   router.post("/login", (req, res) => {
     const userID = req.session.user_id;
 
-    console.log("here");
-
-    if (!userID) {
-      return res.json({
-        auth: false,
-        message: "not logged in"
-      });
-    }
-
     db.getUserById(userID).then((dbusr) => {
+      //the user id from cookie matches an id in the database
+      console.log("db user", dbusr);
       if (dbusr) {
         //user is already logged in
         console.log("already logged in");
-        return res.json({
-          auth: true,
-          message: "already logged in"
-        });
+        return res.json({ auth: true, message: "already logged in" });
       }
 
       //user is not logged in
@@ -55,25 +45,17 @@ module.exports = (db) => {
             if (req.body.password === user.password) {
               //check the user password vs the form password
               req.session.user_id = user.id;
+              console.log(req.session.user_id);
               console.log("authenticated");
-              res.json({
-                auth: true,
-                message: "success"
-              });
+              res.json({ auth: true, message: "success" });
             } else {
               console.log("not authenticated");
-              res.json({
-                auth: false,
-                message: "bad password"
-              });
+              res.json({ auth: false, message: "bad password" });
             }
           } else {
             //user does not exist in db as per email
             console.log("email does not exist");
-            res.json({
-              auth: false,
-              message: "not a valid email address"
-            });
+            res.json({ auth: false, message: "not a valid email address" });
           }
         })
         .catch((err) => {
@@ -82,20 +64,43 @@ module.exports = (db) => {
           console.log(err);
           res
             .status(500)
-            .json({
-              auth: false,
-              message: "internal server error"
-            });
+            .json({ auth: false, message: "internal server error" });
         });
     });
+  });
 
-    router.post("/logout", (req, res) => {
-      req.session = null; //deletes user cookies
-      res.json({
-        auth: false,
-        message: "sucessfully logged out user"
+  router.post("/logout", (req, res) => {
+    req.session = null; //deletes user cookies
+    res.json({ auth: false, message: "sucessfully logged out user" });
+  });
+
+  router.post("/signup", (req, res) => {
+    const user = {
+      first_name: req.body.FirstName,
+      last_name: req.body.FirstName,
+      email: req.body.email,
+      password: req.body.password,
+    };
+    // const values = [user.first_name, user.last_name, user.email, user.password]
+    db.addUser(user)
+      .then((result) => {
+        if (!result) {
+          res.json({
+            auth: false,
+            message: "not succesful in registering user",
+          });
+          return console.log("not successful in adding new user");
+        }
+        console.log("successfully added user to db: ", result);
+        req.session.user_id = result.id; //set the cookie according to the userid returned from the database
+        res.json({ auth: true, message: "succesful registration" });
+      })
+      .catch((err) => {
+        console.log("db error", err);
+        res.status(500).json({ auth: false, message: "internal server error" });
       });
-    });
+
+    // db.addUser()
   });
   return router;
 };
