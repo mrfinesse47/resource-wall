8bbf1be916cb8820f32839391eb855d29b7d6e37
Kevin Mason <krmason2020@gmail.com>
2021-11-28 00:17:16 +0000

    initial steps on setting up routes

--- routes/users.js
+++ routes/users.js
@@ -5,21 +5,35 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const fakeUser = require("../fake-data/user.json");
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users;`)
-      .then(data => {
-        const users = data.rows;
-        res.json({ users });
+    // db.query(`SELECT * FROM users;`)
+    //   .then((data) => {
+    //     // const users = data.rows;
+    //     res.json({ users: "hello" });
+    //   })
+    //   .catch((err) => {
+    //     res.status(500).json({ error: err.message });
+    //   });
+    res.json({ users: "hello" });
+  });
+  router.post("/login", (req, res) => {
+    const { email, password } = req.body;
+    login(email, password)
+      .then((user) => {
+        if (!user) {
+          res.send({ error: "error" });
+          return;
+        }
+        req.session.userId = user.id;
+        res.send({ user: { name: user.name, email: user.email, id: user.id } });
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
+      .catch((e) => res.send(e));
+    res.json({ users: "hello" });
   });
   return router;
 };
