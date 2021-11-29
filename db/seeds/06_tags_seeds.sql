d16cd5d23731451e2936cd17fc9b82b4c4d81118
Jamie <lijamie19828@gmail.com>
2021-11-29 21:19:27 +0000

    fill tag seeds

--- db/seeds/06_tags_seeds.sql
+++ db/seeds/06_tags_seeds.sql
@@ -1,4 +1,15 @@
 INSERT INTO tags (tag, url)
 VALUES ('Javascript', 'https://images.unsplash.com/photo-1579468118864-1b9ea3c0db4a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8amF2YXNjcmlwdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60'),
 ('HTML', 'https://images.unsplash.com/photo-1593720213428-28a5b9e94613?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'),
-('CSS', 'https://media.istockphoto.com/photos/stylesheet-on-a-screen-programming-concept-picture-id1250984508?k=20&m=1250984508&s=170667a&w=0&h=tuvzyc_-Ji3W-sKy5nFn_cPkWJPP-3Uw-bHq8_AKCBM=')
+('CSS', 'https://media.istockphoto.com/photos/stylesheet-on-a-screen-programming-concept-picture-id1250984508?k=20&m=1250984508&s=170667a&w=0&h=tuvzyc_-Ji3W-sKy5nFn_cPkWJPP-3Uw-bHq8_AKCBM='),
+('SQL', 'https://media.istockphoto.com/photos/database-development-picture-id182660832?k=20&m=182660832&s=170667a&w=0&h=5jWYBJ98B0cBWQlx6mmpdZQE78yALeZPsuT2-UEJmvk='),
+('Git', 'https://images.unsplash.com/photo-1618401471353-b98afee0b2eb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Z2l0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60')
+('Biology', 'https://images.unsplash.com/photo-1576086213369-97a306d36557?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c2NpZW5jZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60'),
+('Chemistry', 'https://images.unsplash.com/photo-1603126857599-f6e157fa2fe6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2hlbWlzdHJ5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60'),
+('Physics', 'https://images.unsplash.com/photo-1633493702341-4d04841df53b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHBoeXNpY3N8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60'),
+('English', 'https://images.unsplash.com/photo-1495446815901-a7297e633e8d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Ym9va3N8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60'),
+('Geography', 'https://images.unsplash.com/photo-1619468129361-605ebea04b44?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Z2VvZ3JhcGh5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60'),
+('History', 'https://images.unsplash.com/photo-1509024644558-2f56ce76c490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fGhpc3Rvcnl8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60'),
+('Psychology', 'https://media.istockphoto.com/photos/hands-holding-brain-with-puzzle-paper-cutout-autism-epilepsy-and-picture-id1272508891?b=1&k=20&m=1272508891&s=170667a&w=0&h=JOG99bUYhWHyYsZJ2dQLYIFmfFzwCHAOBCWXGdEiWiY='),
+('Philosphy', 'https://media.istockphoto.com/photos/classic-statues-plato-close-up-picture-id1066705000?b=1&k=20&m=1066705000&s=170667a&w=0&h=PAZ1GSSr071pcJDTQ-4x_q-vpVP04goiVnDaXmRa1F4='),
+('Art', 'https://media.istockphoto.com/photos/paint-on-canvas-coloured-sections-with-bright-shades-picture-id1282500710?k=20&m=1282500710&s=612x612&w=0&h=MGzUL-AeP3cE3o7tDnC1aXt6WZA0-xRgV87aOx92rV8=');
