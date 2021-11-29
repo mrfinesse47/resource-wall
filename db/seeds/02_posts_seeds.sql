b109698692af88dd758868d51b8ec9237fdfffa3
Jamie <lijamie19828@gmail.com>
2021-11-29 23:21:03 +0000

    updated seeds

--- db/seeds/02_posts_seeds.sql
+++ db/seeds/02_posts_seeds.sql
@@ -1,13 +1,13 @@
 INSERT INTO pins (owner_id, title, description, content_type, url ,content, tag, created_at)
 VALUES
-  (10,'Title: Chemistry','This is a description','video','https://www.youtube.com/watch?v=1ueSa6-UqYo','Lorem',7, '2020-08-4T08:00:00.000Z'),
-  (2,'Title: CSS','Flexbox Guide','text','https://css-tricks.com/snippets/css/a-guide-to-flexbox/','Lorem',3, '2020-12-27T08:00:00.000Z'),
-  (8,'Title: Physics','Visualizing gravity','video','https://www.youtube.com/watch?v=MTY1Kje0yLg','Lorem',8, '2020-11-27T08:00:00.000Z'),
-  (12,'Title: Philosophy','Quick video on Plato','video','https://www.youtube.com/watch?v=VDiyQub6vpw','Lorem',13, '2020-06-27T08:00:00.000Z'),
-  (4,'Title: Biology','Image of an animal cell','image','https://timvandevall.com/wp-content/uploads/animal-cell-diagram-1.png','Lorem',6, '2020-04-27T08:00:00.000Z'),
-  (6,'Title: English','My analysis of watchmen','text','https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1442239711l/472331.jpg','Lorem',9, '2021-11-27T08:00:00.000Z'),
+  (10,'Title: Chemistry','This is a description','video','https://www.youtube.com/watch?v=1ueSa6-UqYo','Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam cum, quia dolores qui minima, cumque voluptas iusto corporis expedita repellat ab nisi neque assumenda tempore! Nulla rerum itaque quam quo! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam cum, quia dolores qui minima, cumque voluptas iusto corporis expedita repellat ab nisi neque assumenda tempore! Nulla rerum itaque quam quo!',7, '2020-08-4T08:00:00.000Z'),
+  (2,'Title: CSS','Flexbox Guide','text','https://css-tricks.com/snippets/css/a-guide-to-flexbox/','Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam cum, quia dolores qui minima, cumque voluptas iusto corporis expedita repellat ab nisi neque assumenda tempore! Nulla rerum itaque quam quo!',3, '2020-12-27T08:00:00.000Z'),
+  (8,'Title: Physics','Visualizing gravity','video','https://www.youtube.com/watch?v=MTY1Kje0yLg','Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam cum, quia dolores qui minima, cumque voluptas iusto corporis expedita repellat ab nisi neque assumenda tempore! Nulla rerum itaque quam quo!',8, '2020-11-27T08:00:00.000Z'),
+  (12,'Title: Philosophy','Quick video on Plato','video','https://www.youtube.com/watch?v=VDiyQub6vpw','Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam cum, quia dolores qui minima, cumque voluptas iusto corporis expedita repellat ab nisi neque assumenda tempore! Nulla rerum itaque quam quo!',13, '2020-06-27T08:00:00.000Z'),
+  (4,'Title: Biology','Image of an animal cell','image','https://timvandevall.com/wp-content/uploads/animal-cell-diagram-1.png','Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam cum, quia dolores qui minima, cumque voluptas iusto corporis expedita repellat ab',6, '2020-04-27T08:00:00.000Z'),
+  (6,'Title: English','My analysis of watchmen','text','https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1442239711l/472331.jpg','Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam cum, quia dolores qui minima, cumque voluptas iusto corporis expedita repellat ab nisi neque assumenda tempore! Nulla rerum itaque quam quo! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam cum, quia dolores qui minima, cumque voluptas iusto corporis expedita repellat ab nisi neque assumenda tempore! Nulla rerum itaque quam quo!',9, '2021-11-27T08:00:00.000Z'),
   (3,'Title: History','Entertaining video on the history of the world!','video','shttps://www.youtube.com/watch?v=xuCn8ux2gbs','Lorem',11, '2021-11-05T08:00:00.000Z'),
-  (13,'Title: HTML','HTML Cheat Sheet','image','http://www.cheat-sheets.org/saved-copy/html-cheat-sheet.png','Lorem',2, '2020-12-29T08:00:00.000Z'),
+  (13,'Title: HTML','HTML Cheat Sheet','image','http://www.cheat-sheets.org/saved-copy/html-cheat-sheet.png','Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam cum, quia dolores qui minima, cumque voluptas iusto corporis expedita repellat ab nisi neque assumenda tempore! Nulla rerum itaque quam quo!',2, '2020-12-29T08:00:00.000Z'),
   (15,'Title: Git','Git Cheat Sheet','image','http://rogerdudler.github.io/git-guide/files/git_cheat_sheet.pdf','Lorem',5, '2019-12-27T08:00:00.000Z'),
   (8,'Title: Art','Cool art technique','image','https://images.unsplash.com/photo-1548685913-fe6678babe8d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGFpbnQlMjB0ZXh0dXJlfGVufDB8fDB8fA%3D%3D&w=1000&q=80','Lorem',14, '2018-12-27T08:00:00.000Z'),
   (7,'Title: Javascript','Cool site for coding challenges','text','https://leetcode.com/','Lorem',3, '2018-12-27T08:00:00.000Z'),
@@ -16,8 +16,8 @@ VALUES
   (11,'Title: SQL','PSQL syntax guide','text','https://www.postgresqltutorial.com/','Lorem',4, '2020-04-27T08:00:00.000Z');
 
 INSERT INTO pins (owner_id, title, description, content_type, content, tag, created_at)
-VALUES (12,'Title: Psychology','This is a description','text','Lorem',12, '2020-05-02T08:00:00.000Z'),
-  (6,'Title: SQL','This is a description','text''Lorem',4, '2020-03-27T08:00:00.000Z'),
-  (11,'Title: Psychology','Stanford Prison Experiment','text','Lorem',12, '2019-11-27T08:00:00.000Z'),
-  (8,'Title: English','Analysis of Romeo and Juliet','text','Lorem',9, '2019-01-27T08:00:00.000Z'),
-  (5,'Title: Geography','Review of the water cycle','text','Lorem',10, '2019-12-27T08:00:00.000Z');
+VALUES (12,'Title: Psychology','This is a description','text','Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam cum, quia dolores qui minima, cumque voluptas iusto corporis expedita repellat ab nisi neque assumenda tempore! Nulla rerum itaque quam quo!',12, '2020-05-02T08:00:00.000Z'),
+  (6,'Title: SQL','This is a description','text''Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam cum, quia dolores qui minima, cumque voluptas iusto corporis expedita repellat ab nisi neque assumenda tempore! Nulla rerum itaque quam quo!',4, '2020-03-27T08:00:00.000Z'),
+  (11,'Title: Psychology','Stanford Prison Experiment','text','Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam cum, quia dolores qui minima, cumque voluptas iusto corporis expedita repellat ab nisi neque assumenda tempore! Nulla rerum itaque quam quo!',12, '2019-11-27T08:00:00.000Z'),
+  (8,'Title: English','Analysis of Romeo and Juliet','text','Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam cum, quia dolores qui minima, cumque voluptas iusto corporis expedita repellat ab nisi neque assumenda tempore! Nulla rerum itaque quam quo!',9, '2019-01-27T08:00:00.000Z'),
+  (5,'Title: Geography','Review of the water cycle','text','Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam cum, quia dolores qui minima, cumque voluptas iusto corporis expedita repellat ab nisi neque assumenda tempore! Nulla rerum itaque quam quo!',10, '2019-12-27T08:00:00.000Z');
