e97d737dfd06d56e31658abe7f0edf9ba9df4489
Jamie <lijamie19828@gmail.com>
2021-11-29 22:54:04 +0000

    new seeds in progess, tables refactored

--- db/seeds/06_tags_seeds.sql
+++ db/seeds/06_tags_seeds.sql
@@ -1,5 +1,6 @@
 INSERT INTO tags (tag, url)
-VALUES ('Javascript', 'https://images.unsplash.com/photo-1579468118864-1b9ea3c0db4a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8amF2YXNjcmlwdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60'),
+VALUES
+('Javascript', 'https://images.unsplash.com/photo-1579468118864-1b9ea3c0db4a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8amF2YXNjcmlwdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60'),
 ('HTML', 'https://images.unsplash.com/photo-1593720213428-28a5b9e94613?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'),
 ('CSS', 'https://media.istockphoto.com/photos/stylesheet-on-a-screen-programming-concept-picture-id1250984508?k=20&m=1250984508&s=170667a&w=0&h=tuvzyc_-Ji3W-sKy5nFn_cPkWJPP-3Uw-bHq8_AKCBM='),
 ('SQL', 'https://media.istockphoto.com/photos/database-development-picture-id182660832?k=20&m=182660832&s=170667a&w=0&h=5jWYBJ98B0cBWQlx6mmpdZQE78yALeZPsuT2-UEJmvk='),
@@ -13,3 +14,4 @@ VALUES ('Javascript', 'https://images.unsplash.com/photo-1579468118864-1b9ea3c0d
 ('Psychology', 'https://media.istockphoto.com/photos/hands-holding-brain-with-puzzle-paper-cutout-autism-epilepsy-and-picture-id1272508891?b=1&k=20&m=1272508891&s=170667a&w=0&h=JOG99bUYhWHyYsZJ2dQLYIFmfFzwCHAOBCWXGdEiWiY='),
 ('Philosphy', 'https://media.istockphoto.com/photos/classic-statues-plato-close-up-picture-id1066705000?b=1&k=20&m=1066705000&s=170667a&w=0&h=PAZ1GSSr071pcJDTQ-4x_q-vpVP04goiVnDaXmRa1F4='),
 ('Art', 'https://media.istockphoto.com/photos/paint-on-canvas-coloured-sections-with-bright-shades-picture-id1282500710?k=20&m=1282500710&s=612x612&w=0&h=MGzUL-AeP3cE3o7tDnC1aXt6WZA0-xRgV87aOx92rV8=');
+
