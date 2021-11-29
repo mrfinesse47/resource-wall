d581dd07969bfefe5dfec3dd5e1d6a512e4591e7
Jamie <lijamie19828@gmail.com>
2021-11-29 20:03:21 +0000

    edit pins table, start seed creation for tags

---
+++ db/seeds/06_tags_seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO tags (tag, url)
+VALUES ('Javascript', 'https://images.unsplash.com/photo-1579468118864-1b9ea3c0db4a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8amF2YXNjcmlwdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60'),
+('HTML', 'https://images.unsplash.com/photo-1593720213428-28a5b9e94613?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'),
+('CSS', 'https://media.istockphoto.com/photos/stylesheet-on-a-screen-programming-concept-picture-id1250984508?k=20&m=1250984508&s=170667a&w=0&h=tuvzyc_-Ji3W-sKy5nFn_cPkWJPP-3Uw-bHq8_AKCBM=')
