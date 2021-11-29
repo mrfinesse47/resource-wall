5ae0026dbd59aed07a21183f93f8c76cd3750247
Kevin Mason <krmason2020@gmail.com>
2021-11-29 03:25:26 +0000

    got all pins route working

--- routes/pins.js
+++ routes/pins.js
@@ -4,13 +4,12 @@ const isUserLoggedIn = require("./helpers/isUserLoggedIn");
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    console.log("get pins");
-    res.json({ auth: false });
-
+    console.log("here");
+    const userID = req.session.user_id;
     isUserLoggedIn(userID, db).then((isLoggedIn) => {
       //this is in every route and needs to be its own helper
       //the user id from cookie matches an id in the database
-      console.log("db user", dbusr);
+      //console.log("db user", dbusr);
 
       if (!isLoggedIn) {
         //user is already logged in
@@ -20,6 +19,22 @@ module.exports = (db) => {
           message: "not logged in",
         });
       }
+      //if user is logged in proceed with getting all pins from db
+      db.getAllPins()
+        .then((pins) => {
+          console.log("success sending all pins");
+          res.json({
+            auth: true,
+            message: "successfully got all pins",
+            pins,
+          });
+        })
+        .catch((err) => {
+          res.status(500).json({
+            auth: false,
+            message: "internal server error",
+          });
+        });
     });
   });
   return router;
