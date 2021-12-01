33f60fadda9e623d67aea2008a6e21cebd38c288
Kevin Mason <krmason2020@gmail.com>
2021-12-01 22:41:36 +0000

    got it working again

--- public/scripts/views/pins.js
+++ public/scripts/views/pins.js
@@ -26,69 +26,61 @@ const pins = (obj) => {
         $(this).addClass("fa-heart ");
         $(this).removeClass("fa-heart-o");
 
-        $(`#${$pin.id} img`).click(function (event) {
-          $.ajax({
-              method: "GET",
-              url: `api/pins/${$pin.id}`,
-            })
-            .done(function (obj) {
-              console.log(obj);
-              if (obj.auth) {
-                render("expandedPins", obj);
-              } else {
-                render("login", obj);
-              }
-            })
-            .done(function (obj) {
-              console.log(obj);
-              if (obj.auth) {
-                // render("expandedPins", obj);
-              } else {
-                render("login", obj);
-              }
-            })
-            .fail(function () {
-              //should either render pins or give a notification that logout failed
-            });
-          //going to do a server request here and then toggle its state
-        });
-
-        $(`#${$pin.id} img`).click(function (event) {
-          $.ajax({
-              method: "GET",
-              url: `api/pins/${$pin.id}`,
-            })
-            .done(function (obj) {
-              console.log(obj);
-              if (obj.auth) {
-                render("expandedPins", obj);
-              } else {
-                render("login", obj);
-              }
-            })
-            .fail(function () {
-              //should either render pins or give a notification that logout failed
-            });
-        });
+        // console.log($pin.id);
+        // ${$pin.id}
+        $.ajax({
+          method: "POST",
+          url: `api/pins/favorites/${$pin.id}`,
+        })
+          .done(function (obj) {
+            console.log(obj);
+            if (obj.auth) {
+              // render("expandedPins", obj);
+            } else {
+              render("login", obj);
+            }
+          })
+          .fail(function () {
+            //should either render pins or give a notification that logout failed
+          });
+        //going to do a server request here and then toggle its state
       });
-    }, obj);
 
-    // const loadPins = function () {
-    //   $.ajax({
-    //       method: 'GET',
-    //       url: "api/pins",
-    //       cache: false,
-    //       dataType: "json",
-    //     })
-    //     .done(function (obj) {
-    //       console.log(obj);
-    //       renderPins(obj);
-    //     })
-    //     .fail(function () {
-    //       console.log("something went wrong in loadPins ajax") // should return an error here
-    //     });
-    // };
+      $(`#${$pin.id} img`).click(function (event) {
+        $.ajax({
+          method: "GET",
+          url: `api/pins/${$pin.id}`,
+        })
+          .done(function (obj) {
+            console.log(obj);
+            if (obj.auth) {
+              render("expandedPins", obj);
+            } else {
+              render("login", obj);
+            }
+          })
+          .fail(function () {
+            //should either render pins or give a notification that logout failed
+          });
+      });
+    });
+  }, obj);
 
-    // loadPins();
-  })
-}
+  // const loadPins = function () {
+  //   $.ajax({
+  //       method: 'GET',
+  //       url: "api/pins",
+  //       cache: false,
+  //       dataType: "json",
+  //     })
+  //     .done(function (obj) {
+  //       console.log(obj);
+  //       renderPins(obj);
+  //     })
+  //     .fail(function () {
+  //       console.log("something went wrong in loadPins ajax") // should return an error here
+  //     });
+  // };
+
+  // loadPins();
+};
