bdd51939a8ee41902e529a1ff34969ec25276e00
Jamie <lijamie19828@gmail.com>
2021-12-01 03:22:59 +0000

    merge db-queries, deal with conflicts

--- db/schema/00_tags.sql
+++ db/schema/00_tags.sql
@@ -2,12 +2,6 @@ DROP TABLE IF EXISTS tags CASCADE;
 
 CREATE TABLE tags (
   id SERIAL PRIMARY KEY NOT NULL,
-<<<<<<< HEAD
-  tag VARCHAR(255),
-  thumbnail_url
-)
-=======
   name VARCHAR(255),
   thumbnail_url text
 );
->>>>>>> db-queries
