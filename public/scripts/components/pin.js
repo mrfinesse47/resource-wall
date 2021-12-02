a29ae7eb949ecc30e6e4579beacfc2bd972ef28c
Jonathan Su <jsu6898@gmail.com>
2021-12-01 16:54:48 -0800

    implemented star ratings

--- public/scripts/components/pin.js
+++ public/scripts/components/pin.js
@@ -1,12 +1,61 @@
 const createPinElement = (obj) => {
   let des = obj.description;
   let cont = obj.content;
-  const rating = Math.round(obj.average_rating * 10) / 10;
+  let rating = Math.round(obj.average_rating * 10) / 10;
+
   if (des.length > 50) {
     des = des.slice(0, 50) + "...";
   }
   if (cont.length > 150) {
     cont = cont.slice(0, 150) + "...";
+  }
+
+  let starRating = (rating) => {
+    console.log(rating);
+    if (rating === 5) {
+      return `
+      <span class="fa fa-star checked"></span>
+      <span class="fa fa-star checked"></span>
+      <span class="fa fa-star checked"></span>
+      <span class="fa fa-star checked"></span>
+      <span class="fa fa-star checked"></span>`
+    } else if (rating > 4) {
+      return `
+      <span class="fa fa-star checked"></span>
+      <span class="fa fa-star checked"></span>
+      <span class="fa fa-star checked"></span>
+      <span class="fa fa-star checked"></span>
+      <span class="fa fa-star"></span>`
+    } else if (rating > 3) {
+      return `
+      <span class="fa fa-star checked"></span>
+      <span class="fa fa-star checked"></span>
+      <span class="fa fa-star checked"></span>
+      <span class="fa fa-star"></span>
+      <span class="fa fa-star"></span>`
+    } else if (rating > 2) {
+      return `
+      <span class="fa fa-star checked"></span>
+      <span class="fa fa-star checked"></span>
+      <span class="fa fa-star"></span>
+      <span class="fa fa-star"></span>
+      <span class="fa fa-star"></span>`
+    } else if (rating > 1) {
+      return `
+      <span class="fa fa-star checked"></span>
+      <span class="fa fa-star"></span>
+      <span class="fa fa-star"></span>
+      <span class="fa fa-star"></span>
+      <span class="fa fa-star"></span>`
+    } else if (rating >= 0) {
+      return `
+      <span class="fa fa-star"></span>
+      <span class="fa fa-star"></span>
+      <span class="fa fa-star"></span>
+      <span class="fa fa-star"></span>
+      <span class="fa fa-star"></span>`
+    }
+
   }
 
   //this will be pulled in and assembled from ajax
@@ -31,7 +80,7 @@ const createPinElement = (obj) => {
 
         <span class="rating">
          ${obj.name}
-        ${rating} / 5
+        ${starRating(rating)}
         </span>
 
         <span>
