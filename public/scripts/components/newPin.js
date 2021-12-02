1e91d1ac87eb67e3185812950aa32c9f26d8dc78
Jonathan Su <jsu6898@gmail.com>
2021-12-02 10:32:24 -0800

    used bootstrap to make the dropdown

--- public/scripts/components/newPin.js
+++ public/scripts/components/newPin.js
@@ -16,20 +16,10 @@ const createNewPinElement = () => {
   <input type="text" class="form-control" id="create-description" name="description" placeholder="Description goes here">
 </div>
 
-
 <div class="mb-3">
-  <label for="create-img" class="form-label">Thumbnail</label>
-  <input type="url" class="form-control" id="create-img" name="thumbnail_url"  placeholder="optional img url">
-</div>
-
-
-<div class="mb-3">
-  <label for="create-content" class="form-label">Content</label>
-  <textarea class="form-control" id="create-content" name="content" rows="3"></textarea>
-</div>
-
-<label for="tags">Choose a tag:</label>
-<select name="tag" id="tag_dropdown">
+<label for="select-tag" class="form-label">Choose a tag</label>
+<select class="form-select" name="tag" id="select-tag" aria-label="Default select example">
+  <option selected>Choose a tag:</option>
   <option value="1">Computer Science</option>
   <option value="2">Biology</option>
   <option value="3">Chemistry</option>
@@ -41,6 +31,12 @@ const createNewPinElement = () => {
   <option value="9">Philosophy </option>
   <option value="10">Art</option>
 </select>
+</div>
+
+<div class="mb-3">
+  <label for="create-content" class="form-label">Content</label>
+  <textarea class="form-control" id="create-content" name="content" rows="3"></textarea>
+</div>
 
 <div class="mb-3" id="create-box">
 <button type="submit" id="createPinBtn">Create</button>
